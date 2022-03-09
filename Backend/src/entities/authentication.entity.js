c4eafcbf3ed0aa2c971676e92f1f4298cf60550d
DESKTOP-4H3CT4H\Hong Tham <thamb1505745@gmail.com>
2022-03-09 10:09:59 +0700

    xoa backend

--- Backend/src/entities/authentication.entity.js
+++
@@ -1,9 +0,0 @@
-const { BaseEntity } = require("./bases/base.entity");
-const { Sequelize } = require('sequelize');
-
-class Authentication extends BaseEntity {
-    
-    username = { type: Sequelize.STRING(50) };
-    password = { type: Sequelize.STRING(50) };
-}
-module.exports = { Authentication }
